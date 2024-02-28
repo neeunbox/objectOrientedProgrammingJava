2d0bf4693f9e8c6ef794e8b07ab0ee15fe0944b5
neeunbox <neeunbox@gmail.com>
2024-02-29 06:44:37 +0900

    Create new oop java project

---
+++ OOPJavaPrj/src/ex01/code/reuse/Program.java
@@ -0,0 +1,14 @@
+package ex01.code.reuse;
+
+import ex04.Isolate.ui.code.Exam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Exam exam = new Exam(1,1,1);
+		
+		System.out.println(exam.getTotal());
+
+	}
+
+}
